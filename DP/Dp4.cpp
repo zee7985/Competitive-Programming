28a66dd4dfac986969f4fc8aff771620d6bbd5bb
Zeeshan <53146361+zee7985@users.noreply.github.com>
2021-01-13 17:17:37 +0530

    LCS ,subs,Occ of gfg two string equal(insert,del,up)

--- DP/Dp4.cpp
+++ DP/Dp4.cpp
@@ -207,6 +207,27 @@ int minCostInDeRecu(string str1, string str2, int i, int j)
                    )); 
 } 
 
+ int minCostDP(string str1, string str2) {
+        int m = str1.size(), n = str2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
 //DP
 
   
